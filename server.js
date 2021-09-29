5741b9eae023f5bca8a62d13bb553944943135bf
nivshitrit <80672827+nivshitrit@users.noreply.github.com>
2021-09-29 14:24:06 +0300

    c

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.post('/count', (req, res) => {
 });
 app.get('/', (req, res) => res.sendFile(path.resolve("pages/index.html")));
 const PORT = process.env.PORT || 3000;
-app.listen(3000);
+app.listen(PORT);
